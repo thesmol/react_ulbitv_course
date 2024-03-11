ca1812607ff527dff69c17b05458d7368e5111a5
Медведев Иван <ranzar2000@gmail.com>
2024-03-12 00:54:14 +0300

    add Post component to the App

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
+// import Button from '@mui/material/Button';
+import Post from "./components/Post";
+import "./styles/App.css";
 
 function App() {
 
   return (
     <div className='App'>
+      <Post />
     </div>
   )
 }
