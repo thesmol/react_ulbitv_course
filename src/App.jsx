6e795b8e0091eda2beba37bfa1e3ea767f8befa0
Медведев Иван <ranzar2000@gmail.com>
2024-03-12 00:53:59 +0300

    Remove unused counters and items

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,8 @@
-import { useState } from 'react';
-import './App.css';
-import Counter from './components/Counter';
-import ClassCounter from './components/ClassCounter';
 
 function App() {
-  const [value, setValue] = useState("Состояние в элементе");
-
 
   return (
     <div className='App'>
-      <Counter />
-      <ClassCounter />
-      <h1>{value}</h1>
-      <input
-        type='text'
-        value={value}
-        onChange={event => setValue(event.target.value)}
-      />
     </div>
   )
 }
