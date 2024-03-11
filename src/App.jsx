4ef849d4c84ce378a5b4d0ac6a7dfaab60274f4f
Медведев Иван <ranzar2000@gmail.com>
2024-03-12 00:20:38 +0300

    Implement the counter component using the function

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,15 @@
 import { useState } from 'react';
 import './App.css';
+import Counter from './components/Counter';
 
 function App() {
-  const [count, setCount] = useState(0);
   const [value, setValue] = useState("Состояние в элементе");
-
-  function handleIncrement() {
-    setCount(count + 1);
-  }
-
-  function handleDecrement() {
-    setCount(count - 1);
-  }
 
 
   return (
     <div className='App'>
-      <h1>{count}</h1>
-      <button onClick={handleIncrement}>
-        Increment
-      </button>
-
-      <button onClick={handleDecrement}>
-        Decrement
-      </button>
-
+      <Counter />
+      
       <h1>{value}</h1>
       <input
         type='text'
