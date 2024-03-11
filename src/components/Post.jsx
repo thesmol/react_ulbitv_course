a87f47015f1ef77d70e9fa9f64424ab50c2107a7
Медведев Иван <ranzar2000@gmail.com>
2024-03-12 00:52:54 +0300

    create Post component

---
+++ src/components/Post.jsx
@@ -0,0 +1,15 @@
+export default function Post() {
+    return (
+        <div className='post'>
+            <div className='post__content'>
+                <strong>1. JavaScript</strong>
+                <div>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam necessitatibus quaerat officiis voluptate praesentium sit consequuntur illum nemo qui, deleniti, id earum dolorem nostrum laboriosam ad porro cum vero placeat?
+                </div>
+            </div>
+            <div className="post__buttons">
+                <button>Удалить</button>
+            </div>
+        </div>
+    )
+}
