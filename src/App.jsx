4c96b64c7965e819c1e2a8c75b68f13ec45a693a
Медведев Иван <ranzar2000@gmail.com>
2024-03-13 01:17:20 +0300

    Create a simple API and use it in the App

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 // import Button from '@mui/material/Button';
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./styles/App.css";
 import PostList from "./components/PostList";
 import PostForm from "./components/PostForm";
@@ -7,10 +7,10 @@ import PostFilter from "./components/PostFilter";
 import MyModal from "./components/UI/MyModal/MyModal";
 import MyButton from "./components/UI/button/MyButton";
 import { usePost } from "./hooks/usePosts";
+import PostService from "./API/PostService";
 
 function App() {
   const [posts, setPosts] = useState([]);
-
   const [filter, setFilter] = useState({
     sort: '',
     query: ''
@@ -20,6 +20,19 @@ function App() {
 
   const sortedAndSearchedPosts = usePost(posts, filter.sort, filter.query);
 
+  // подгрузит посты при заходе в приложение
+  useEffect(() => {
+    fetchPosts();
+  }, []);
+
+  async function fetchPosts() {
+    setIsPostsLoading(true);
+    setTimeout(async () => {
+      const posts = await PostService.getAll();
+      setPosts(posts);
+      setIsPostsLoading(false);
+    }, 1000);
+  }
   const createPost = (newPost) => {
     setPosts([...posts, newPost])
   }
