fada24cd4b277af68d9f80e3cc4f1a874048edd9
Медведев Иван <ranzar2000@gmail.com>
2024-03-12 17:34:22 +0300

    Add a form to add new posts

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@
 import { useState } from "react";
 import "./styles/App.css";
 import PostList from "./components/PostList";
+import MyButton from "./components/UI/button/MyButton";
+import MyInput from "./components/UI/input/MyInput";
 
 function App() {
   const [posts, setPosts] = useState([
@@ -11,30 +13,45 @@ function App() {
     { id: 4, title: 'JavaScript 4', body: 'lorem ipsum' },
   ]);
 
-  const [posts2, setPosts2] = useState([
-    { id: 1, title: 'React', body: 'lorem ipsum' },
-    { id: 2, title: 'React 2', body: 'lorem ipsum' },
-    { id: 3, title: 'React 3', body: 'lorem ipsum' },
-    { id: 4, title: 'React 4', body: 'lorem ipsum' },
-  ]);
+  const [title, setTitle] = useState("");
+  const [desctiption, setDesctiption] = useState("");
+
+  const handleNewPost = (event) => {
+    event.preventDefault();
+
+    const newPost = {
+      id: Date.now(),
+      title,
+      body: desctiption
+    };
+
+    setPosts([...posts, newPost]);
+    setTitle("");
+    setDesctiption("");
+  }
 
   return (
     <div className='App'>
-        {/* Неуправляемый (неконтролируемый) компонент */}
+      <form>
+        {/* управляемый компонент */}
         <MyInput
-          ref={bobyInputRef}
+          value={title}
+          onChange={event => setTitle(event.target.value)}
+          type="text"
+          placeholder="Заголовок поста"
+        />
+        <MyInput
+          value={desctiption}
+          onChange={event => setDesctiption(event.target.value)}
           type="text"
           placeholder="Описание поста"
         />
         <MyButton onClick={handleNewPost}>Добавить пост</MyButton>
       </form>
+
       <PostList
         posts={posts}
         title="Списков постов 1"
-      />
-      <PostList
-        posts={posts2}
-        title="Списков постов 2"
       />
     </div>
   )
