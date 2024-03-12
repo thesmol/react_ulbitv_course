46584c759e30508278657ea79bc9926ec905ed05
Медведев Иван <ranzar2000@gmail.com>
2024-03-12 19:53:54 +0300

    style mySelect

--- src/components/UI/select/MySelect.jsx
+++ src/components/UI/select/MySelect.jsx
@@ -1,8 +1,10 @@
 import PropTypes from 'prop-types';
+import classes from './MySelect.module.css';
 
 function MySelect({ options, defaultValue, value, onChange }) {
     return (
         <select
+            className={classes.mySelect}
             value={value}
             onChange={event => onChange(event.target.value)}
         >
