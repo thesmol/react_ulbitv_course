086cfe04ec7c407333ef4096055bb9a320496351
Медведев Иван <ranzar2000@gmail.com>
2024-03-13 19:54:50 +0300

    add a logout button to navbar

--- src/components/UI/navbar/Navbar.jsx
+++ src/components/UI/navbar/Navbar.jsx
@@ -1,13 +1,29 @@
 import { Link } from "react-router-dom";
 import "../../../styles/App.css";
+import MyButton from "../button/MyButton";
+import { useContext } from "react";
+import { AuthContext } from "../../../context";
 
 function Navbar() {
+    const {setIsAuth} = useContext(AuthContext);
+
+    const logout = () => {
+        setIsAuth(false);
+        localStorage.removeItem('auth');
+    }
+
     return (
         <div className="navbar">
+
             <div className="navbar__links">
                 <Link to="/about">О нас</Link>
                 <Link to="/posts">Посты</Link>
             </div>
+            <MyButton
+                onClick = {logout}
+            >
+                Выйти
+            </MyButton>
         </div>
     )
 }
