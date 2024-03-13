bf7b00eab2bc8379cf2fe72d578f1f9151b6bb16
Медведев Иван <ranzar2000@gmail.com>
2024-03-13 15:19:18 +0300

    build basic navigation structure via BrowserRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
 // import Button from '@mui/material/Button';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./styles/App.css";
+import Posts from "./pages/Posts";
+import About from "./pages/About";
 
 
+function App() {
   return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="posts" element={<Posts />} />
+        <Route path="about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
