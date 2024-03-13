af628603f1445402381df3fcb3627dadfc6a3678
Медведев Иван <ranzar2000@gmail.com>
2024-03-13 17:33:02 +0300

    Move routes to component AppRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
 // import Button from '@mui/material/Button';
-import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import "./styles/App.css";
-import Posts from "./pages/Posts";
-import About from "./pages/About";
-import Error from "./pages/Error";
 import Navbar from "./components/UI/navbar/Navbar";
+import AppRouter from "./components/AppRouter";
 
 
 function App() {
@@ -12,12 +10,7 @@ function App() {
     <BrowserRouter>
       <Navbar />
 
-      <Routes>
-        <Route path="posts" element={<Posts />} />
-        <Route path="about" element={<About />} />
-        <Route path="*" element={<Error />} />
-        {/* <Route path="*" element={<Navigate to="posts" replace />} /> */}
-      </Routes>
+      <AppRouter />
     </BrowserRouter>
   )
 }
