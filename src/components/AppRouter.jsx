a916b97326e37b60dd6886eafd7c1d6de6099641
Медведев Иван <ranzar2000@gmail.com>
2024-03-13 18:23:38 +0300

    add a route for specific posts

--- src/components/AppRouter.jsx
+++ src/components/AppRouter.jsx
@@ -2,13 +2,16 @@ import { Route, Routes } from "react-router-dom";
 import Posts from "../pages/Posts";
 import About from "../pages/About";
 import Error from "../pages/Error";
+import PostIdPage from "../pages/PostIdPage";
 
 function AppRouter() {
     return (
         <Routes>
-            <Route path="posts" element={<Posts />} />
+            <Route exact path="posts" element={<Posts />} />
+            <Route exact path="posts/:id" element={<PostIdPage />} />
             <Route path="about" element={<About />} />
             <Route path="*" element={<Error />} />
+            
         </Routes>
     )
 }
