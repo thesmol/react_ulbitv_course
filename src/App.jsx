e00c91e94558d3a26a5d729266c21aaac7dde26b
Медведев Иван <ranzar2000@gmail.com>
2024-03-13 17:30:40 +0300

    Create a navbar component and add it to the App

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,14 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./styles/App.css";
 import Posts from "./pages/Posts";
 import About from "./pages/About";
+import Navbar from "./components/UI/navbar/Navbar";
 
 
 function App() {
   return (
     <BrowserRouter>
+      <Navbar />
+
       <Routes>
         <Route path="posts" element={<Posts />} />
         <Route path="about" element={<About />} />
