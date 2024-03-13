3c0f9c74f9da1c5cdb6fd3e2b70fb518da9546d2
Медведев Иван <ranzar2000@gmail.com>
2024-03-13 14:14:49 +0300

    modify the ClassName pass to MyButton

--- src/components/UI/button/MyButton.jsx
+++ src/components/UI/button/MyButton.jsx
@@ -3,16 +3,17 @@ import PropTypes from 'prop-types';
 
 // css module создает уникальное классовое название 
 
-const MyButton = ({children, ...props}) => {
+const MyButton = ({ children, ...props }) => {
   return (
-    <button {...props} className={classes.myBtn}>
+    <button {...props} className={[classes.myBtn, props.className].join(" ")}>
       {children}
     </button>
   )
 };
 
 MyButton.propTypes = {
-    children: PropTypes.string
+  children: PropTypes.string,
+  className: PropTypes.string,
 };
 
 export default MyButton;
